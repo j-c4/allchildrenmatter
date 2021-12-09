f848caab319bf002e0c5c6210a737db33397f84f
João Cipriano <joao.correia.cipriano@gmail.com>
2021-12-09 19:49:52 +0000

    MainView done without background image

--- src/main/java/org/academiadecodigo/hackathon/allchildrenmatter/Controlador.java
+++ src/main/java/org/academiadecodigo/hackathon/allchildrenmatter/Controlador.java
@@ -1,25 +1,29 @@
 package org.academiadecodigo.hackathon.allchildrenmatter;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controlador {
 
-   @GetMapping
-    public String test (){
-       return "intro";
-   }
+    @GetMapping
+    public String test() {
+        return "mainView";
+    }
 
-   @GetMapping(path = "/level1")
-    public String test2(){
-       return "level1";
-   }
+    @GetMapping(path = "/intro")
+    public String showIntro() {
+        return "intro";
+    }
 
-   @GetMapping(path = "/wrong")
-    public String wrong(){
-       return "wrong";
-   }
+    @GetMapping(path = "/level1")
+    public String test2() {
+        return "level1";
+    }
+
+    @GetMapping(path = "/wrong")
+    public String wrong() {
+        return "wrong";
+    }
 }
 
