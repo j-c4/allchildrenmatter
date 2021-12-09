8c76b1ae72426c0e763cfb3f5237a0e13dad124f
Code Cadet <codecadet@enterprise.local>
2021-12-09 19:23:54 +0000

    level 1 to level 3

--- src/main/java/org/academiadecodigo/hackathon/allchildrenmatter/Controlador.java
+++ src/main/java/org/academiadecodigo/hackathon/allchildrenmatter/Controlador.java
@@ -12,14 +12,39 @@ public class Controlador {
        return "intro";
    }
 
+    @GetMapping(path = "/wrong")
+    public String wrong(){
+        return "wrong";
+    }
+
    @GetMapping(path = "/level1")
-    public String test2(){
+    public String level1(){
        return "level1";
    }
+    @GetMapping(path = "/level2")
+    public String level2(){
+        return "level2";
+    }
 
-   @GetMapping(path = "/wrong")
-    public String wrong(){
-       return "wrong";
-   }
+    @GetMapping(path = "/level3")
+    public String level3(){
+        return "level3";
+    }
+
+    @GetMapping(path = "/level4")
+    public String level4(){
+        return "level4";
+    }
+
+    @GetMapping(path = "/level5")
+    public String level5(){
+        return "level5";
+    }
+
+    @GetMapping(path = "/level6")
+    public String level6(){
+        return "level6";
+    }
+
 }
 
