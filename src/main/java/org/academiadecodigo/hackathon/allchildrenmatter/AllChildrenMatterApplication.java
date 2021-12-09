9149782633aa5d5415398279ddecdf97c03696d8
Tiago Moreira <tiagorajaomoreira@gmail.com>
2021-12-09 15:17:45 +0000

    Added git ignore

---
+++ src/main/java/org/academiadecodigo/hackathon/allchildrenmatter/AllChildrenMatterApplication.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.hackathon.allchildrenmatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllChildrenMatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllChildrenMatterApplication.class, args);
+	}
+
+}
